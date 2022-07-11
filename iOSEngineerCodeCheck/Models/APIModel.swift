0210ebbca8ce5b56781c748c05b0739eca1a1318
iori <yzhongtian194@gmail.com>
2022-07-11 21:40:00 +0900

    [add] func getRepositoryを追加
    ViewControllerファイルに存在するRepositoryのデータを受け取る処理をAPIModelに追加し、FatVCを改善する

--- iOSEngineerCodeCheck/Models/APIModel.swift
+++ iOSEngineerCodeCheck/Models/APIModel.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+class API {
+    func getRepository(word: String?, repo: [[String: Any]], tableView: UITableView) {
+        guard let word = word else { return }
+        let url = URL(string: "https://api.github.com/search/repositories?q=\(word)") //URLを生成
+        guard let url = url else { return }
+        let request = URLRequest(url: url)  //Requestを生成
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
+            guard let data = data else { return }
+            do {
+                guard let objectOptional = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let object = objectOptional as? [[String: Any]] else { return }    // DataをJsonに変換
+                
+                var repos = [Repository]()
+                for r in object {
+                    let repository = Repository(attributes: r)
+                    repos.append(repository)
+                }
+                DispatchQueue.main.async {
+                    tableView.reloadData()
+                }
+                
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
