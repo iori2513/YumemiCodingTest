8c2bc598fb1f4614fe0e9d449044abb1db6bced6
iori <yzhongtian194@gmail.com>
2022-07-17 18:41:37 +0900

    [add] 検索欄が空白ならアラートを表示する機能を追加

--- iOSEngineerCodeCheck/Controllers/SearchRepositoryVC.swift
+++ iOSEngineerCodeCheck/Controllers/SearchRepositoryVC.swift
@@ -22,18 +22,35 @@ class SearchRepositoryVC: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let word = searchBar.text
+        var word = searchBar.text
+        word?.removeAll(where: {$0 == " "}) //検索欄の空白を取り除く
         let api = API()
         guard let word = word else { return }
-        api.getData(for: word, success: { [weak self] (repos) in //循環参照を防ぐ
+        if word.isEmpty {
+            //検索欄が空白のみならアラート表示
+            alert()
+        } else {
+            //Repositoryの検索をかける
+            api.getData(for: word, success: { [weak self] (repos) in //循環参照を防ぐ
+                guard let self = self else { return }
+                self.repos = repos
+                self.tableView.reloadData()
+                return
+            }, failure: {(error) in
+                print(error)
+                return
+            })
+        }
+    }
+    
+    private func alert() {
+        let alert = UIAlertController(title: "エラー", message: "文字を入力してください", preferredStyle: .alert)
+        let button = UIAlertAction(title: "OK", style: .default) { [weak self] (action) in
             guard let self = self else { return }
-            self.repos = repos
-            self.tableView.reloadData()
-            return
-        }, failure: {(error) in
-            print(error)
-            return
-        })
+            self.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(button)
+        present(alert, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
